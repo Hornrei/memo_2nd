a636a5470392d9e6518ff88fefa4182bc4372e1b
hornrei <hornrei0808@gmail.com>
2023-05-08 14:55:17 +0900

    更新

---
+++ PY/01相羽珠音.py
@@ -0,0 +1,17 @@
+# year = []
+# for i in range(2022,2201):
+#   if i % 4 == 0 and not(i % 100 == 0 and i % 400 != 0):
+#     year .append(i)
+# print(year)
+
+
+
+year1 = []
+for j in range(2022,2201):
+  if j % 4 == 0 and not(j % 100 == 0 and j % 400 != 0):
+    year1.append(j)
+
+# for i in year1:
+#   print(i)
+
+
